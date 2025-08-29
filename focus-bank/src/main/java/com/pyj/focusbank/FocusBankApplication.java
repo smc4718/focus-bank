0fa9b022cd073b756be6f9e7e366655d89484244
smc4718 <smc4718@naver.com>
2025-08-29 20:01:44 +0900

    스프링부트 프로젝트 설정,gitignore 추가

---
+++ focus-bank/src/main/java/com/pyj/focusbank/FocusBankApplication.java
@@ -0,0 +1,13 @@
+package com.pyj.focusbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FocusBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FocusBankApplication.class, args);
+	}
+
+}
---
+++ focus-bank/src/test/java/com/pyj/focusbank/FocusBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pyj.focusbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FocusBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
